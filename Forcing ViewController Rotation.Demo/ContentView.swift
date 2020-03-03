e94702154654eacacecbee5a413b4f4ee1ab2099
ArturoArriaga <arturoarriaga.speech@gmail.com>
2020-03-03 13:44:18 -0600

    Initial Commit

---
+++ Forcing ViewController Rotation.Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Forcing ViewController Rotation.Demo
+//
+//  Created by Art Arriaga on 3/3/20.
+//  Copyright © 2020 ArturoArriaga.IO. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
